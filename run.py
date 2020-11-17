e213058a1e4b327c8f1787342d940f118e632f91
Edis Kazaz <edis.kazaz@gmail.com>
2020-11-17 02:53:36 +0100

    added results

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ def home():
 @app.route("/<oib_mbs>", methods=["POST", "GET"])
 def user(oib_mbs):
     data = json.loads(zvajzer(oib_mbs), encoding="'UTF-8'")
-    return jsonify(data)
+    return render_template("index.html", results=jsonify(data))
 
 
 if __name__ == "__main__":
